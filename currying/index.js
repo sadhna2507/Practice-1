aaad985cd9f440f8bbabf6139cfd19776cd60095
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-08-25 23:43:25 +0530

    practice

--- currying/index.js
+++ currying/index.js
@@ -1,14 +1,24 @@
 //currying
-function main(a,b,c,d){
-    return a+b+c+d;
-}
+// function main(a,b,c,d){
+//     return a+b+c+d;
+// }
 
-function main(a){
-    return (b) =>{
-        return(c) =>{
-            return(d) =>{
-                return a+b+c+d
-            }
-        }
+// function main(a){
+//     return (b) =>{
+//         return(c) =>{
+//             return(d) =>{
+//                 return a+b+c+d
+//             }
+//         }
+//     }
+// }
+
+
+function main(){
+    return(b) =>{
+        if(b) return main(a+b)
+
+        returna
     }
 }
+console.log(main(1)(5)(9)(14))

be5105db940d1811d9f99c6c71ff670179c49a2f
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-08-26 19:22:58 +0530

    weather

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -18,9 +18,12 @@ btn1.addEventListener("click", function(name){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=5142a079c21f6cb2df156c3a42bfec63`)
     .then(response => response.json()) //text ---> json
     .then(data =>{
-        let result = `<div><h1></h1>city : ${data.name}</div>`
+        let result = `<div><h1>city : ${data.name}</h1>
+        <h1>temperature : ${Math.floor(data["main"].temp-273)} °C</h1>
+        <h1>longitude : ${data["coord"].lon}</h1>
+        <h1>longitude : ${data["coord"].lat}</h1></div>`
         let container = document.querySelector(".container")
-        container.innerHTML = result
+        container.innerHTML += result
         console.log(data);
     }).catch(err => alert("Invalid city name!!"))
 })

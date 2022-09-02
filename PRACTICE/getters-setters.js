bfef56d1e383b6582aab0367ae39e49da5ec06e0
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-09-02 23:44:25 +0530

    practice

--- PRACTICE/getters-setters.js
+++ PRACTICE/getters-setters.js
@@ -1,14 +1,33 @@
 //GETTERS AND SETTERS IN JAVASCRIPT
 
+// const person = {
+//     firstName : "sadhna",
+//     lastName : "Khairia",
+//     fullName(){
+//         // return `${firstName} ${lastName}` //---------->will not work
+//         return `${person.firstName} ${person.lastName}` //------------> will work
+
+//     }
+// }
+// person.fullName = "sachnin tendulkar"
+// console.log(person)
+// console.log(person.fullName())  //----->not wrking
+
+
+
+
 const person = {
     firstName : "sadhna",
     lastName : "Khairia",
-    fullName(){
-        // return `${firstName} ${lastName}` //---------->will not work
-        return `${person.firstName} ${person.lastName}` //------------> will work
-
-    }
+    get fullName(){
+            return `${person.firstName} ${person.lastName}`
+    },
+    
+    set fullName (newName){
+        let parts = newName.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    },
 }
 person.fullName = "sachnin tendulkar"
-console.log(person)
-// console.log(person.fullName())  //----->not wrking
+console.log(person.fullName)
